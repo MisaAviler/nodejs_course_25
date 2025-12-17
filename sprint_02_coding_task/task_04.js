31c50f96a25e92d44944416c335edca9ddf0de04
MisaAvilova <ler.avilova1998@gmail.com>
2025-12-17 14:21:32 +0200

    added first two tasks from the course

---
+++ sprint_02_coding_task/task_04.js
@@ -0,0 +1,11 @@
+// Task 04
+
+// Напишіть модуль який отримує аргумент - шлях до файлу files/sci_fi_4.csv і повертає масив масивів рядків. Тобто:
+// [
+//  ["Atlas Protocol", 2004, 4.0, ],
+
+
+
+
+export async function task_04(pathToFile) {
+}
---
+++ sprint_02_coding_task/task_08.js
@@ -0,0 +1,7 @@
+// Task 08
+
+// Напишіть модуль який отримує аргумент - шлях до файлу files/sci_fi_8.csv і повертає масив об'єктів. 
+
+
+export async function task_08(pathToFile) {
+}
